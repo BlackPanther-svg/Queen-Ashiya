48df051ee69fb6cb14e30fd7c781e2dbafffa7f9
Shashika21 <102890059+Shashika21@users.noreply.github.com>
2022-07-01 13:27:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -39,17 +39,17 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.owner = ['Didula Rashmika🌹⃝⃘̉̉̉̉̉̉🧚']
+global.owner = ['Nimesh SNR🌹⃝⃘̉̉̉̉̉̉🧚']
 
-global.premium = ['🖤Black Panther Bot🖤']
+global.premium = ['🖤Nimesh SNR🖤']
 
-global.ownernomer = '+94741671668'
+global.ownernomer = '94740219072'
 
-global.ownername = 'Didula Rashmika🌹⃝⃘̉̉̉̉̉̉🧚'
+global.ownername = 'Nimesh SNR🌹⃝⃘̉̉̉̉̉̉🧚'
 
-global.botname = '🖤Black Panther Bot🖤'
+global.botname = '🖤Nimesh SNR Bot🖤'
 
-global.footer = '©Black panther🖤.'
+global.footer = '©Nimesh SNR🖤.'
 
 global.ig = 'https://github.com/BlackPanther-svg/BLACK-PANTHER-MD-V2'
 
@@ -57,7 +57,7 @@ global.region = '*╚═══❖•ೋ° 𝙱𝚕𝚊𝚌𝚔 𝙿𝚊𝚗𝚝�
 
 global.sc = 'https://github.com/BlackPanther-svg/BLACK-PANTHER-MD-V2'
 
-global.myweb = 'https://youtube.com/channel/UChP2lplECA2eZGCYLy_QeRA'
+global.myweb = 'https://youtube.com/channel/UChP2lplECA2eCYLy_QeRA'
 
 global.packname = '🖤Black Panther Bot🖤'
 
