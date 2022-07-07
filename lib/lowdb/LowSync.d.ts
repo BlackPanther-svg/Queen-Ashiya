a61b29210c9c2cad6188cce1ef8311e78be16cfd
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-07-07 12:23:03 +0530

    Update LowSync.d.ts

--- lib/lowdb/LowSync.d.ts
+++ lib/lowdb/LowSync.d.ts
@@ -1,1 +1,21 @@
+export interface SyncAdapter<T> {
 
+    read: () => T | null;
+
+    write: (data: T) => void;
+
+}
+
+export declare class LowSync<T = unknown> {
+
+    adapter: SyncAdapter<T>;
+
+    data: T | null;
+
+    constructor(adapter: SyncAdapter<T>);
+
+    read(): void;
+
+    write(): void;
+
+}
