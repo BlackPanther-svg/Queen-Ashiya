1ccb72307a8bec39d0981aef5bb4fc6dafaff1bb
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-07-07 13:01:34 +0530

    Update TextFile.d.ts

--- lib/lowdb/adapters/TextFile.d.ts
+++ lib/lowdb/adapters/TextFile.d.ts
@@ -1,1 +1,15 @@
+import { Adapter } from '../Low.js';
 
+export declare class TextFile implements Adapter<string> {
+
+    private filename;
+
+    private writer;
+
+    constructor(filename: string);
+
+    read(): Promise<string | null>;
+
+    write(str: string): Promise<void>;
+
+}
