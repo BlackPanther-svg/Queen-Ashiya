7bdd037121f291386310ddf957ced49e9435bf3d
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-07-07 13:00:30 +0530

    Update MemorySync.d.ts

--- lib/lowdb/adapters/MemorySync.d.ts
+++ lib/lowdb/adapters/MemorySync.d.ts
@@ -1,1 +1,13 @@
+import { SyncAdapter } from '../LowSync.js';
+
+export declare class MemorySync<T> implements SyncAdapter<T> {
+
+    private data;
+
+    read(): T | null;
+
+    write(obj: T): void;
+
+}
+
 
