ebcf1ea05f1a0f8f4bea75f85137e2fa72a3f412
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-07-07 12:59:21 +0530

    Update Memory.d.ts

--- lib/lowdb/adapters/Memory.d.ts
+++ lib/lowdb/adapters/Memory.d.ts
@@ -1,1 +1,11 @@
+import { Adapter } from '../Low.js';
 
+export declare class Memory<T> implements Adapter<T> {
+
+    private data;
+
+    read(): Promise<T | null>;
+
+    write(obj: T): Promise<void>;
+
+}
