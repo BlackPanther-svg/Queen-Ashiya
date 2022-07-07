3cd0f448e963023323aee6b00b44769e99809232
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-07-07 12:58:15 +0530

    Update LocalStorage.d.ts

--- lib/lowdb/adapters/LocalStorage.d.ts
+++ lib/lowdb/adapters/LocalStorage.d.ts
@@ -1,1 +1,15 @@
 
+
+import { SyncAdapter } from '../LowSync.js';
+
+export declare class LocalStorage<T> implements SyncAdapter<T> {
+
+    private key;
+
+    constructor(key: string);
+
+    read(): T | null;
+
+    write(obj: T): void;
+
+}
