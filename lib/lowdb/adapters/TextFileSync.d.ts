84bfcc66ab22de9315005e00d820faac4dd28a14
Sohan Matheesha <105502291+Mathiya578@users.noreply.github.com>
2022-07-07 13:02:55 +0530

    Update TextFileSync.d.ts

--- lib/lowdb/adapters/TextFileSync.d.ts
+++ lib/lowdb/adapters/TextFileSync.d.ts
@@ -1,1 +1,15 @@
+import { SyncAdapter } from '../LowSync.js';
 
+export declare class TextFileSync implements SyncAdapter<string> {
+
+    private tempFilename;
+
+    private filename;
+
+    constructor(filename: string);
+
+    read(): string | null;
+
+    write(str: string): void;
+
+}
